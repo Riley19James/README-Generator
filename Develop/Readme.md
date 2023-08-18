3154d7a56eaab8175327befd42c1bf381e6b0cba
Riley19James <rikennedy@live.com>
2023-08-18 09:54:10 -0300

    Created functions and markdown files for READEME Challenge.

--- Develop/Readme.md
+++ Develop/Readme.md
@@ -1,2 +1,16 @@
-# README.md
+# Fresh-Finder
+![Licence](https://img.shields.io/badge/license-afl-3.0-brightgreen)
 
+## Description
+
+Grocery store website that will give you fresh produce and fish at best price!
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#License)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+If you have questions, feel free to contact me on my Github account! [GitHub](https://github.com/Riley19James)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,49 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![Licence](https://img.shields.io/badge/license-${license}-brightgreen)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `[Licence](./LICENCE)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## Licence 
+    This Project is Licensed unde the [${license}](./LICENCE) license.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#License)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+If you have questions, feel free to contact me on my Github account! [GitHub](https://github.com/Riley19James)
 `;
 }
 
